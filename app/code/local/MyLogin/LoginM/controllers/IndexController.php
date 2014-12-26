fa104bff53f916d53d66661336e5d851866b57d6
root <root@localhost.localdomain>
2014-12-26 18:31:53 +0800

    Coding110 <coding110@163.com>

--- app/code/local/MyLogin/LoginM/controllers/IndexController.php
+++ app/code/local/MyLogin/LoginM/controllers/IndexController.php
@@ -1,12 +1,9 @@
 <?php
 
-class MyLogin_LoginM_IndexController extends Mage_Core_Controller_Front_Action{
+class Sm_BasicProducts_IndexController extends Mage_Core_Controller_Front_Action{
 	public function IndexAction() {
 		echo  'fejifejifejfiefie' ;
-	}
-	
-	public function addAction(){
-		echo 'add';
+		
 	}
 }
 ?>
